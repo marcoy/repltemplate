94b19dd4d4f916c56e81e72da333d267a274d46f
Marco Yuen <marcoy@gmail.com>
2014-11-10 23:03:26 -0500

    Initial commit

---
+++ resources/leiningen/new/repltemplate/repl.clj
@@ -0,0 +1,3 @@
+(ns {{name}}.repl
+  (:require [clojure.repl :refer :all]
+            [clojure.tools.namespace.repl :refer [refresh]]))
---
+++ src/leiningen/new/repltemplate.clj
@@ -0,0 +1,20 @@
+(ns leiningen.new.repltemplate
+  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
+            [leiningen.core.main :as main]))
+
+(def render (renderer "repltemplate"))
+
+(defn repltemplate
+  "My normal workflow"
+  [name]
+  (let [data {:name name
+              :sanitized (name-to-path name)}]
+    (main/info "Generating fresh 'lein new' repltemplate project.")
+    (->files data
+             ["LICENSE" (render "LICENSE" data)]
+             [".gitignore" (render "gitignore" data)]
+             ["project.clj" (render "project.clj" data)]
+             ["README.md" (render "README.md" data)]
+             ["src/{{sanitized}}/core.clj" (render "core.clj" data)]
+             ["src/{{sanitized}}/repl.clj" (render "repl.clj" data)]
+             ["test/{{sanitized}}/core_test.clj" (render "core_test.clj" data)])))
