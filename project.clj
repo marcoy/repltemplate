871e22c26634625186f83d9614c1098d99d3fe59
Marco Yuen <marcoy@gmail.com>
2014-11-10 23:32:18 -0500

    Remove SNAPSHOT from version string

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject repltemplate/lein-template "0.1.0-SNAPSHOT"
+(defproject repltemplate/lein-template "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
