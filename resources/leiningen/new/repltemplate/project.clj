85958d83a13a8e57a6618eea8a052fcc05ae4ef1
Marco Yuen <marcoy@gmail.com>
2015-01-03 13:05:21 -0500

    Upgrade tools.namespace

--- resources/leiningen/new/repltemplate/project.clj
+++ resources/leiningen/new/repltemplate/project.clj
@@ -12,5 +12,5 @@
 
   :dependencies [[aprint "0.1.1"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.namespace "0.2.7"]
+                 [org.clojure/tools.namespace "0.2.8"]
                  [slingshot "0.12.1"]])
