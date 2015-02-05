6dc4b779e0c56db15d0de07074c05324d053f87d
Marco Yuen <marcoy@gmail.com>
2015-02-05 15:35:12 -0500

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject repltemplate/lein-template "0.1.1"
+(defproject repltemplate/lein-template "0.1.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
